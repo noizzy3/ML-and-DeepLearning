bf7e9b0fce263079f1800d5a414b0749296e2365
noizzy3 <subbham@gmail.com>
2016-10-12 01:11:49 +0530

    sentic_net

---
+++ NLP_senticnet/senticnet_verb.py
@@ -0,0 +1,68 @@
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import sentiwordnet as swn
+import numpy as np
+from senti_classifier import senti_classifier
+from senti_classifier.senti_classifier import synsets_scores
+import difflib
+sm = difflib.SequenceMatcher(None)
+#print synsets_scores[syn.name()]['pos']
+target=open("verbs.txt",'w')
+def check(a,b):
+	if(len(a) > 2 and len(b)>2):
+		if a[0] == b[0] and a[1] == b[1] and a[2] == b[2] :
+			return 0
+	return 1
+
+
+positive = ["#joyful" , "#interesting" , "#surprising" , "#admirable"]
+negative = ["#sad", "#scared" , "#angry" , "#disgusting"]
+
+#change below manually
+
+positive_form = ['joy','interest','surprise','adore']
+negative_form = ['lament','frighten','inflame','disgust']
+
+target.write("senticnet = {}\n")
+for syn in list(wn.all_synsets('v')):
+	s = str(syn.lemmas()[0].name())
+	sm.set_seq2(s)
+	synonyms = []
+	score = []
+	if synsets_scores[syn.name()]['pos'] == 0.0 and synsets_scores[syn.name()]['neg'] == 0.0:
+		continue
+
+	if synsets_scores[syn.name()]['pos'] >= synsets_scores[syn.name()]['neg'] :
+		for x in positive_form :
+			sm.set_seq1(x)
+			score.append(sm.ratio())
+		synonyms.append(positive[np.argmax(score)])
+		score[np.argmax(score)] = -10
+		synonyms.append(positive[np.argmax(score)])
+	else :
+		for x in negative_form :
+			sm.set_seq1(x)
+			score.append(sm.ratio())
+		synonyms.append(negative[np.argmax(score)])
+		score[np.argmax(score)] = -10
+		synonyms.append(negative[np.argmax(score)])
+
+	for l in syn.lemmas() :
+		w=nltk.stem.WordNetLemmatizer().lemmatize(str(l.name())) #lemmatize the word
+		if check(s,w) :
+			synonyms.append(w) #str converts it from unicode to string
+		if len(synonyms) > 6 :
+			break
+	if len(synonyms) == 7 :
+		target.write(s)
+		target.write(" = [\"")
+		count=0
+		for x in synonyms:
+			if count :
+				target.write(",\"")
+			count+=1
+			target.write(x)
+			target.write("\"")
+		target.write("]")
+		target.write("\n")
+target.close()
---
+++ multilayer_neuralnet/love_returns.py
@@ -0,0 +1,79 @@
+#t=love.network([784,30,10],30,10,3.0)
+
+import random
+import cPickle
+import gzip
+import numpy as np
+
+class network(object):
+	def __init__(self,sizes,epochs,minibatch,eta):
+		
+		f=gzip.open('mnist.pkl.gz','rb')
+		trd,vd,td=cPickle.load(f)
+		f.close()
+		tri=[np.reshape(x,(784,1)) for x in trd[0]]
+		trv=[self.vectorise(x) for x in trd[1]]
+		training_data=zip(tri,trv)
+		ti=[np.reshape(x,(784,1)) for x in td[0]]
+		test_data=zip(ti,td[1])
+
+		self.size=sizes
+		self.w=[np.random.randn(y,x) for y,x in zip(sizes[1:], sizes[:-1])]
+		self.b=[np.random.randn(x,1) for x in self.size[1:]]
+		for outer in range(epochs):
+			random.shuffle(training_data)#list is shuffled
+			for inner in range(0,len(training_data),minibatch)	:
+				nabla_w=[np.zeros((y,x)) for y,x in zip(sizes[1:], sizes[:-1])]
+				nabla_b=[np.zeros((x,1)) for x in self.size[1:]]
+				for k in range(inner,inner+minibatch):
+					a=training_data[k][0]
+					val=training_data[k][1]# 10 X 1 array
+					activation=[a]#list containing final activation
+					for i in range(len(self.size)-1):
+						a=self.sigmoid(i,a)
+						activation.append(a)
+
+					dw,db=self.backprop(activation,val)
+					nabla_w=[x+y for x,y in zip(nabla_w,dw)]
+					nabla_b=[x+y for x,y in zip(nabla_b,db)]
+
+				"The below update takes place for each minibatch "
+					
+				self.w=[x-(eta/minibatch)*y for x,y in zip(self.w,nabla_w)]
+				self.b=[x-(eta/minibatch)*y for x,y in zip(self.b,nabla_b)]
+			
+			self.test(test_data,outer+1)
+	
+
+	def sigmoid(self,i,act):
+		z = np.dot(self.w[i],act) + self.b[i]
+		return (np.exp(2*z)-1)/(np.exp(2*z)+1)
+
+
+	def backprop(self,activation,val): #For one training data
+		db=[np.zeros(b.shape) for b in self.b]
+		dw=[np.zeros(w.shape) for w in self.w]
+		c=activation[-1]-val
+		for i in range(len(self.size)-2,-1,-1):
+			c=c*(1-activation[i+1]*activation[i+1])
+			db[i]=c
+			dw[i]=np.dot(c,activation[i].transpose())
+			c=np.dot(self.w[i].transpose(),c)
+
+		return (dw,db)
+
+
+	def test(self,t,e):
+		c=0
+		for i,j in t:
+			ar=i
+			for k in range(len(self.size)-1):
+				ar=self.sigmoid(k,ar)
+			c=c+int(np.argmax(ar)==j)
+		print "epoch: %d    accuracy: %f percent" %(e,c/100.0)
+
+
+	def vectorise(self,y):
+		z=np.zeros((10,1))
+		z[y]=1
+		return z
